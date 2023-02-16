cca8dea479c1c978fe0adee667eed65dd33ed2d4
Sanskar-Adhikari <adhikarisanskar99@gmail.com>
2023-02-15 23:27:25 -0800

    frit2

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,15 @@
-import {configureStore, combineReducers, applyMiddleware} from "redux";
-import thunk from "redux-thunk";
-import {composeWithDevTools} from "redux-devtools-extension";
+import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { productReducer } from './reducers/productReducer';
 
+const store = configureStore({
+  reducer: {
+    // combine your reducers here
+    products:productReducer,
+  },
+  middleware: [thunk],
+  devTools: true, // enable the Redux DevTools browser extension
+});
 
+export default store;
