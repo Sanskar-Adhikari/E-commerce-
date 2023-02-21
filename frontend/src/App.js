675a82f68b41ca345fc9141d5f518ccd76578784
Sanskar-Adhikari <adhikarisanskar99@gmail.com>
2023-02-20 16:52:17 -0800

    asd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
-import Header from "./component/Header/Header"
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
-import Footer from "./component/Footer/Footer"
-import Home from "./component/Home/Home"
-import ProductDetails from "./component/Product/ProductDetails"
+import Header from './component/Header/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './component/Footer/Footer';
+import Home from './component/Home/Home';
+import ProductDetails from './component/Product/ProductDetails';
+
 function App() {
   return (
     <Router>
-      <Header/>
+      <Header />
       <Routes>
-        <Route exact path='/' element={<Home/>}/>  
-        <Route exact path="/products/:id" component={<ProductDetails/>} />
-      </Routes>
-      <Footer/>
+        <Route exact path='/' element={<Home />} />
+        <Route path="/product/:id" element = {<ProductDetails />}/>
+ 
+     </Routes>
+      <Footer />
     </Router>
   );
 }
--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -1,34 +1,38 @@
 import React, { useEffect, Fragment } from 'react';
-import Carousel from 'react-material-ui-carousel'
-import {useDispatch,useSelector} from "react-redux"
-import {getProductDetails} from "../../actions/ProductActions"
-import "./ProductDetails.css"
+import Carousel from 'react-material-ui-carousel';
+import { useDispatch, useSelector } from 'react-redux';
+import { getProductDetails } from '../../actions/ProductActions';
+import './ProductDetails.css';
 
+const ProductDetails = ({ match }) => {
+    console.log(match);
+  const dispatch = useDispatch();
+  const { product, loading, error } = useSelector(
+    (state) => state.productDetails
+  );
 
+  useEffect(() => {
+    dispatch(getProductDetails(match.params.id));
+  }, [dispatch, match.params.id]);
 
-const ProductDetails = ({match}) => {
-
-    const dispatch= useDispatch();
-    const {product, loading, error} = useSelector(state => state.productDetails);
-    useEffect(()=>{
-        dispatch(getProductDetails(match.params.id))
-    },[dispatch,match.params.id]);
-
-
-    return (
-        <Fragment>
-            <div className='ProductDetails'>
-                <div>
-                    <Carousel>
-                        {product.images&&product.images.map((item,i)=>(
-                            <img className='CarouselImage' key={item.url} src={item.url} alt={`${i}Slide`}/> 
-                        ))}
-                    </Carousel>
-                </div>
-            </div>
-            </Fragment>
-
-    );
-}
+  return (
+    <Fragment>                   
+      <div className='ProductDetails'>
+        <h1>{product.name}</h1>
+        <Carousel>
+          {product.images &&
+            product.images.map((item, i) => (
+              <img
+                className='CarouselImage'
+                key={item.url}
+                src={item.url}
+                alt={`${i}Slide`}
+              />
+            ))}
+        </Carousel>
+      </div>
+    </Fragment>
+  );
+};
 
 export default ProductDetails;
