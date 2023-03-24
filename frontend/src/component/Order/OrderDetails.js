e81a94f4a4c5d89ed54958b07156cbcf607edda1
Sanskar-Adhikari <adhikarisanskar99@gmail.com>
2023-03-23 22:12:40 -0700

    added comments for ordersa

--- frontend/src/component/Order/OrderDetails.js
+++ frontend/src/component/Order/OrderDetails.js
@@ -1,130 +1,160 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { Typography } from "@material-ui/core";
 import { getOrderDetails, clearErrors } from "../../actions/orderAction";
 import { useAlert } from "react-alert";
 import "./orderDetails.css";
-import { useParams } from 'react-router-dom';
-import LoadingScreen from '../LoadingComponent/LoadingScreen';
-import TopHeading from '../TopHeading';
+import { useParams } from "react-router-dom";
+import LoadingScreen from "../LoadingComponent/LoadingScreen";
+import TopHeading from "../TopHeading";
 
 
+/**/
+/*
+OrderDetails
+NAME
+    OrderDetails
+SYNOPSIS
+    OrderDetails()
+DESCRIPTION
+    The OrderDetails component displays the details of a particular order
+    such as the order number, shipping information, payment details,
+    order status, and the list of ordered items.
+RETURNS
+    This component returns a JSX element for displaying order details
+*/
+/**/
 const OrderDetails = () => {
-    const dispatch = useDispatch();
-    const alert = useAlert();
-    const { order, error, loading } = useSelector((state) => state.orderDetails);
-    const { id } = useParams();
+  // Importing the necessary hooks
+  const dispatch = useDispatch();
+  const alert = useAlert();
 
-    useEffect(() => {
-        if (error) {
-          alert.error(error);
-          dispatch(clearErrors());
-        }
-    
-        dispatch(getOrderDetails(id));
-      }, [dispatch, alert, error, id]);
-  
-    return (
+  // Destructuring the required values from the Redux store state object.
+  const { order, error, loading } = useSelector((state) => state.orderDetails);
+  const { id } = useParams();
+
+  // Using the useEffect hook to trigger an action when the component mounts or updates.
+  useEffect(() => {
+    // If there is an error, display it using the alert function and clear the error from the Redux store.
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+
+    // Dispatch the getOrderDetails action to get the details of the order with the specified ID.
+    dispatch(getOrderDetails(id));
+  }, [dispatch, alert, error, id]);
+
+  return (
     <Fragment>
-        {loading ? (<LoadingScreen/>):
-        (
-            <Fragment>
-                <TopHeading title="Order Details"/>
-                <div className="orderDetailsPage">
-            <div className="orderDetailsContainer">
-              <Typography component="h1">
-                Order #{order && order._id}
-              </Typography>
-              <Typography>Shipping Info</Typography>
-              <div className="orderDetailsContainerBox">
-                <div>
-                  <p>Name:</p>
-                  <span>{order.user && order.user.name}</span>
+      {
+        // if loading, render the LoadingScreen component, otherwise render the order details.
+        loading ? (
+          <LoadingScreen />
+        ) : (
+          <Fragment>
+
+            {/* Display the page heading */}
+            <TopHeading title="Order Details" />
+            <div className="orderDetailsPage">
+              <div className="orderDetailsContainer">
+                <Typography component="h1">
+
+                  {/* Display the order ID */}
+                  Order #{order && order._id}
+                </Typography>
+
+                {/* Display the shipping information */}
+                <Typography>Shipping Info</Typography>
+                <div className="orderDetailsContainerBox">
+                  <div>
+                    <p>Name:</p>
+                    <span>{order.user && order.user.name}</span>
+                  </div>
+                  <div>
+                    <p>Phone:</p>
+                    <span>
+                      {order.shippingInfo && order.shippingInfo.phoneNo}
+                    </span>
+                  </div>
+                  <div>
+                    <p>Address:</p>
+                    <span>
+                      {order.shippingInfo &&
+                        `${order.shippingInfo.address}, ${order.shippingInfo.city}, ${order.shippingInfo.state}, ${order.shippingInfo.pinCode}, ${order.shippingInfo.country}`}
+                    </span>
+                  </div>
                 </div>
-                <div>
-                  <p>Phone:</p>
-                  <span>
-                    {order.shippingInfo && order.shippingInfo.phoneNo}
-                  </span>
-                </div>
-                <div>
-                  <p>Address:</p>
-                  <span>
-                    {order.shippingInfo &&
-                      `${order.shippingInfo.address}, ${order.shippingInfo.city}, ${order.shippingInfo.state}, ${order.shippingInfo.pinCode}, ${order.shippingInfo.country}`}
-                  </span>
-                </div>
-              </div>
-              <Typography>Payment</Typography>
-              <div className="orderDetailsContainerBox">
-                <div>
-                  <p
-                    className={
-                      order.paymentInfo &&
+
+                {/* Display the payment information */}
+                <Typography>Payment</Typography>
+                <div className="orderDetailsContainerBox">
+                  <div>
+                    <p
+                      className={
+                        order.paymentInfo &&
+                        order.paymentInfo.status === "succeeded"
+                          ? "greenColor"
+                          : "redColor"
+                      }
+                    >
+                      {order.paymentInfo &&
                       order.paymentInfo.status === "succeeded"
-                        ? "greenColor"
-                        : "redColor"
-                    }
-                  >
-                    {order.paymentInfo &&
-                    order.paymentInfo.status === "succeeded"
-                      ? "Paid"
-                      : "Not Paid"}
-                  </p>
+                        ? "Paid"
+                        : "Not Paid"}
+                    </p>
+                  </div>
+
+                  <div>
+                    <p>Amount:</p>
+                    <span>{order.totalPrice && order.totalPrice}</span>
+                  </div>
                 </div>
 
-                <div>
-                  <p>Amount:</p>
-                  <span>{order.totalPrice && order.totalPrice}</span>
-                </div>
-              </div>
-
-              <Typography>Order Status</Typography>
-              <div className="orderDetailsContainerBox">
-                <div>
-                  <p
-                    className={
-                      order.orderStatus && order.orderStatus === "Delivered"
-                        ? "greenColor"
-                        : "redColor"
-                    }
-                  >
-                    {order.orderStatus && order.orderStatus}
-                  </p>
+                {/* Display the order status */}
+                <Typography>Order Status</Typography>
+                <div className="orderDetailsContainerBox">
+                  <div>
+                    <p
+                      className={
+                        order.orderStatus && order.orderStatus === "Delivered"
+                          ? "greenColor"
+                          : "redColor"
+                      }
+                    >
+                      {order.orderStatus && order.orderStatus}
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className="orderDetailsCartItems">
-              <Typography>Order Items:</Typography>
-              <div className="orderDetailsCartItemsContainer">
-                {order.orderItems &&
-                  order.orderItems.map((item) => (
-                    <div key={item.product}>
-                      <img src={item.image} alt="Product" />
-                      <Link to={`/product/${item.product}`}>
-                        {item.name}
-                      </Link>{" "}
-                      <span>
-                        {item.quantity} X ${item.price} ={" "}
-                        <b>${item.price * item.quantity}</b>
-                      </span>
-                    </div>
-                  ))}
+              {/* Display the order items */}
+              <div className="orderDetailsCartItems">
+                <Typography>Order Items:</Typography>
+                <div className="orderDetailsCartItemsContainer">
+                  {order.orderItems &&
+                    order.orderItems.map((item) => (
+                      <div key={item.product}>
+                        <img src={item.image} alt="Product" />
+                        <Link to={`/product/${item.product}`}>
+                          {item.name}
+                        </Link>{" "}
+                        <span>
+                          {item.quantity} X ${item.price} ={" "}
+                          <b>${item.price * item.quantity}</b>
+                        </span>
+                      </div>
+                    ))}
+                </div>
               </div>
             </div>
-          </div>
+          </Fragment>
+        )
+      }
+    </Fragment>
+  );
+};
 
-
-
-                </Fragment>
-
-        )}
-        
-
-        </Fragment>
-  )
-}
-
-export default OrderDetails
+/* OrderDetails() */
+export default OrderDetails;
--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -1,51 +1,81 @@
-import React, { useEffect, Fragment, useState } from 'react';
-import Carousel from 'react-material-ui-carousel';
-import { useDispatch, useSelector } from 'react-redux';
-import { clearErrors, getProductDetails, newReview } from '../../actions/ProductActions';
-import './ProductDetails.css';
-import { useParams } from 'react-router-dom';
-import ReviewCard from "./ReviewCard"
-import LoadingScreen from '../LoadingComponent/LoadingScreen';
+import React, { useEffect, Fragment, useState } from "react";
+import Carousel from "react-material-ui-carousel";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  clearErrors,
+  getProductDetails,
+  newReview,
+} from "../../actions/ProductActions";
+import "./ProductDetails.css";
+import { useParams } from "react-router-dom";
+import ReviewCard from "./ReviewCard";
+import LoadingScreen from "../LoadingComponent/LoadingScreen";
 import { useAlert } from "react-alert";
-import TopHeading from '../TopHeading';
-import {addItemsToCart} from "../../actions/cartAction";
-import {Dialog,DialogActions, DialogContent,DialogTitle,Button } from "@material-ui/core"
-import {Rating} from "@material-ui/lab"
-import { NEW_REVIEW_RESET } from '../../constants/productConstants';
+import TopHeading from "../TopHeading";
+import { addItemsToCart } from "../../actions/cartAction";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Button,
+} from "@material-ui/core";
+import { Rating } from "@material-ui/lab";
+import { NEW_REVIEW_RESET } from "../../constants/productConstants";
 
+
+/**/
+/*
+ProductDetails
+NAME
+    ProductDetails
+SYNOPSIS
+    ProductDetails()
+DESCRIPTION
+    Displays the details of a product, including its name, images,
+    price, description, reviews, and a button to add the product to the cart. It also allows users to submit
+    reviews for the product.
+RETURNS
+    JSX Element to display product details
+*/
+/**/
 const ProductDetails = () => {
+  // Importing the necessary hooks
   const alert = useAlert();
-
   const dispatch = useDispatch();
+
+  // Selecting product details from the Redux store
   const { product, loading, error } = useSelector(
     (state) => state.productDetails
   );
 
+  // Getting the product ID from the URL params
   const { id } = useParams();
 
+  // Selecting new review details from the Redux store
   const { success, error: reviewError } = useSelector(
     (state) => state.newReview
   );
 
-
+  // useEffect hook to handle side effects when the component mounts and updates
   useEffect(() => {
-    if(error)
-    {
-       alert.error(error);
-       dispatch(clearErrors());
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
     }
-    if(reviewError)
-    {
-       alert.error(reviewError);
-       dispatch(clearErrors());
+    if (reviewError) {
+      alert.error(reviewError);
+      dispatch(clearErrors());
     }
     if (success) {
       alert.success("Review Submitted Successfully");
-      dispatch({ type: NEW_REVIEW_RESET});
+      dispatch({ type: NEW_REVIEW_RESET });
     }
+    // Dispatch an action to fetch the product details from the server
     dispatch(getProductDetails(id));
   }, [alert, dispatch, error, id, reviewError, success]);
 
+  // Config options for the rating component
   const options = {
     size: "large",
     value: product.ratings,
@@ -53,17 +83,19 @@ const ProductDetails = () => {
     precision: 0.5,
   };
 
-  const[quantity,setQuantity]= useState(1);
+  //initializing use state
+  const [quantity, setQuantity] = useState(1);
   const [open, setOpen] = useState(false);
   const [rating, setRating] = useState(0);
   const [comment, setComment] = useState("");
 
+  // Toggles the visibility of the review form
+  const submitReviewToggel = () => {
+    open ? setOpen(false) : setOpen(true);
+  };
 
-  const submitReviewToggel=()=>{
-    open? setOpen(false):setOpen(true);
-  }
-
-  const reviewSubmitHandler=()=>{
+  // Submits a new review to the server
+  const reviewSubmitHandler = () => {
     const myForm = new FormData();
     myForm.set("rating", rating);
     myForm.set("comment", comment);
@@ -71,123 +103,153 @@ const ProductDetails = () => {
     dispatch(newReview(myForm));
 
     setOpen(false);
+  };
 
-  }
-
-  const increaseQuantity=()=>{
-    if(product.Stock<=quantity)
-      {
-       return;
-      }
-    const temp= quantity+1;
+  // Increases the quantity of products to add to the cart
+  const increaseQuantity = () => {
+    if (product.Stock <= quantity) {
+      return;
+    }
+    const temp = quantity + 1;
     setQuantity(temp);
-  }
-  
-  const decreaseQuantity=()=>{
-    if(quantity<=1)
-    {
-     return;
+  };
+
+  // Decreases the quantity of products to add to the cart
+  const decreaseQuantity = () => {
+    if (quantity <= 1) {
+      return;
     }
 
-  const temp= quantity-1;
-  setQuantity(temp);
-  }
+    const temp = quantity - 1;
+    setQuantity(temp);
+  };
 
-  const addToCartHandler = () =>{
-    dispatch(addItemsToCart(id, quantity))
+  //adds product to the cart
+  const addToCartHandler = () => {
+    dispatch(addItemsToCart(id, quantity));
     alert.success("Item added to cart");
-
-
-  }
+  };
   return (
-  <Fragment>
-    {loading? <LoadingScreen/>:
-    (  <Fragment>  
-      <TopHeading title={`${product.name} DETAIL`}/>                 
-      <div className='ProductDetails'>
-       
-        <Carousel>
-          {product.images &&
-            product.images.map((item, i) => (
-              <img
-                className='CarouselImage'
-                key={item.url}
-                src={item.url}
-                alt={`${i}Slide`}
-              />
-            ))}
-        </Carousel>
-      
-  
-      <div>
-        <div className='detailsBlock-1'>
-          <h2>{product.name}</h2>
-          <p>Product # {product._id}</p>
-        </div>
-        <div className='detailsBlock-2'>
-          <Rating {...options}/>
-          <span className='detailsBlock-2-span'> ({product.numberOfReviews} Reviews)</span>
-        </div>
-        <div className='detailsBlock-3'>
-          <h1>{`$${product.price}`}</h1>
-          <div className='detailsBlock-3-1'>
-            <div className='detailsBlock-3-1-1'>
-              <button onClick={decreaseQuantity}> -</button>
-              <input readOnly value={quantity} type="number" />
+    <Fragment>
+      {/* Check if the page is still loading */}
+      {loading ? (
+        <LoadingScreen />
+      ) : (
+        <Fragment>
+          {/* Display the product details */}
+          <TopHeading title={`${product.name} DETAIL`} />
+          <div className="ProductDetails">
+            <Carousel>
+              {product.images &&
+                product.images.map((item, i) => (
+                  <img
+                    className="CarouselImage"
+                    key={item.url}
+                    src={item.url}
+                    alt={`${i}Slide`}
+                  />
+                ))}
+            </Carousel>
 
-              <button onClick={increaseQuantity}> +</button>
-            </div>
-            <button disabled = {product.Stock<1?true:false} onClick={addToCartHandler}>Add to Cart</button>
-          </div>
-          <p>
-            Status:
-            <b className={product.Stock<1 ? "redColor" : "greenColor"}>{product.Stock<1? "OutOfStock":"InStock"}</b>
-          </p>
-        </div>
-        <div className='detailsBlock-4'>
-          Description: <p>{product.description} </p>
-        </div>
-        <button onClick={submitReviewToggel}   className='submitReview'>Submit Review</button>
-      </div>
-      </div>
+            {/* Display the product information */}
+            <div>
+              <div className="detailsBlock-1">
+                <h2>{product.name}</h2>
+                <p>Product # {product._id}</p>
+              </div>
+              <div className="detailsBlock-2">
+                <Rating {...options} />
+                <span className="detailsBlock-2-span">
+                  {" "}
+                  ({product.numberOfReviews} Reviews)
+                </span>
+              </div>
+              <div className="detailsBlock-3">
+                <h1>{`$${product.price}`}</h1>
+                <div className="detailsBlock-3-1">
+                  <div className="detailsBlock-3-1-1">
+                    {/* Buttons to adjust the quantity of the product */}
+                    <button onClick={decreaseQuantity}> -</button>
+                    <input readOnly value={quantity} type="number" />
 
-      <h3 className='reviewsHeading'>Product Reviews</h3>
-          <Dialog aria-labelledby='simple-dialog-title'
-          open={open} onClose={submitReviewToggel}>
+                    <button onClick={increaseQuantity}> +</button>
+                  </div>
 
-              <DialogTitle>
+                  {/* Button to add the product to the cart */}
+                  <button
+                    disabled={product.Stock < 1 ? true : false}
+                    onClick={addToCartHandler}
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+
+                {/* Display the stock status of the product */}
+                <p>
+                  Status:
+                  <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
+                    {product.Stock < 1 ? "OutOfStock" : "InStock"}
+                  </b>
+                </p>
+              </div>
+              <div className="detailsBlock-4">
+                {/* Display the product description */}
+                Description: <p>{product.description} </p>
+              </div>
+
+              {/* Button to submit a review */}
+              <button onClick={submitReviewToggel} className="submitReview">
                 Submit Review
-              </DialogTitle>
-              <DialogContent className='submitDialog'>
-                <Rating onChange={(e)=>setRating(e.target.value)}
-                value={rating} size="large" />
-                <textarea className='submitDialogTextArea'
-                cols="30" rows="5" value={comment}
-                onChange={(e)=>setComment(e.target.value)}></textarea>
-            
-              </DialogContent>
-              <DialogActions>
-                <Button onClick={reviewSubmitHandler}>Submit</Button>
-                <Button onClick={submitReviewToggel}>Cancel</Button>
-              </DialogActions>
+              </button>
+            </div>
+          </div>
 
+          {/* Display the product reviews */}
+          <h3 className="reviewsHeading">Product Reviews</h3>
+
+          {/* Dialog box to submit a review */}
+          <Dialog
+            aria-labelledby="simple-dialog-title"
+            open={open}
+            onClose={submitReviewToggel}
+          >
+            <DialogTitle>Submit Review</DialogTitle>
+            <DialogContent className="submitDialog">
+              <Rating
+                onChange={(e) => setRating(e.target.value)}
+                value={rating}
+                size="large"
+              />
+              <textarea
+                className="submitDialogTextArea"
+                cols="30"
+                rows="5"
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+              ></textarea>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={reviewSubmitHandler}>Submit</Button>
+              <Button onClick={submitReviewToggel}>Cancel</Button>
+            </DialogActions>
           </Dialog>
 
-
-
-
-
-
-      {product.reviews && product.reviews[0]?(
-        <div className='reviews'>
-        {product.reviews&&product.reviews.map((review)=><ReviewCard key={review._id} review={review}/>)}
-        </div>
-      ):(
-        <p className='noReviews'>No Reviews Yet</p>
+          {/* Display the product reviews*/}
+          {product.reviews && product.reviews[0] ? (
+            <div className="reviews">
+              {product.reviews &&
+                product.reviews.map((review) => (
+                  <ReviewCard key={review._id} review={review} />
+                ))}
+            </div>
+          ) : (
+            <p className="noReviews">No Reviews Yet</p>
+          )}
+        </Fragment>
       )}
-    </Fragment>)}
-  </Fragment>
+    </Fragment>
   );
 };
 
+/* ProductDetails() */
 export default ProductDetails;
